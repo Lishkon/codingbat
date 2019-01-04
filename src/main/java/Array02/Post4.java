4919560d25ba269763413a1e79fbd6b82536a4e6
lishkon <lishkon@gmail.com>
2019-01-05 00:06:50 +0200

    Post4 is solved

--- src/main/java/Array02/Post4.java
+++ src/main/java/Array02/Post4.java
@@ -1,4 +1,22 @@
 package Array02;
 
 public class Post4 {
+
+    /*
+    * Given a non-empty array of ints, return a new array containing the elements from the original array that comes
+    * after the last 4 in the original array. The original array will contain at least one 4.
+    * Note that it is valid in java to create an array of length 0.
+    * */
+    public static int[] post4(int[] nums) {
+        int pos = 0;
+        for (int i = 0; i <= nums.length - 1; i++) {
+            if (nums[i]==4) pos = i;
+        }
+        int[] res = new int[nums.length - (pos+1)];
+        for (int i = 0, j = pos+1; i < res.length; i++, j++) {
+            res[i] = nums[j];
+        }
+        return res;
+    }
+
 }
--- src/test/java/Array02Test/Post4Test.java
+++ src/test/java/Array02Test/Post4Test.java
@@ -1,4 +1,16 @@
 package Array02Test;
 
+import Array02.Post4;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Post4Test {
+    @Test
+    public void sampleTest() {
+        Post4 p4 = new Post4();
+        Assert.assertArrayEquals(new int[]{1,2}, p4.post4(new int[]{2, 4, 1, 2}));
+        Assert.assertArrayEquals(new int[]{2}, p4.post4(new int[]{4, 1, 4, 2}));
+        Assert.assertArrayEquals(new int[]{1, 2, 3}, p4.post4(new int[]{4, 4, 1, 2, 3}));
+        Assert.assertArrayEquals(new int[]{}, p4.post4(new int[]{4}));
+    }
 }
