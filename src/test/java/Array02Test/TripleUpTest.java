d8784207da4be49ce08f2a6beacf4fb22c3b5ee0
lishkon <lishkon@gmail.com>
2019-01-08 18:01:59 +0200

    TripleUpTest uploaded

--- src/test/java/Array02Test/TripleUpTest.java
+++ src/test/java/Array02Test/TripleUpTest.java
@@ -1,4 +1,16 @@
 package Array02Test;
 
+import Array02.TripleUp;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TripleUpTest {
+    @Test
+    public void sampleTest() {
+        TripleUp tu = new TripleUp();
+        Assert.assertTrue(tu.tripleUp(new int[]{1, 4, 5, 6, 2}));
+        Assert.assertTrue(tu.tripleUp(new int[]{1, 2, 3}));
+        Assert.assertFalse(tu.tripleUp(new int[]{1, 3, 4}));
+
+    }
 }
