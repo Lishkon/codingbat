8df8836e395f5818d81c4ed3dbc3ec3e5e1d5839
lishkon <lishkon@gmail.com>
2019-02-19 02:27:35 +0200

    SameStarChar is solved

--- src/main/java/String02/SameStarChar.java
+++ src/main/java/String02/SameStarChar.java
@@ -1,4 +1,31 @@
 package String02;
 
 public class SameStarChar {
+  /*
+   * Returns true if for every '*' (star) in the string, if there are chars both immediately
+   * before and after the star, they are the same.
+   *
+   * sameStarChar("xy*yzz") → true
+   * sameStarChar("xy*zzz") → false
+   * sameStarChar("*xa*az") → true
+   *
+   * Credit @javaproblems.com
+   **/
+  public boolean sameStarChar(String str) {
+    int len = str.length();
+    boolean found = true;
+
+    for (int i = 0; i < len; i++) {
+      String tmpString = str.substring(i,i+1);
+
+      if (tmpString.equals("*") && i > 0 && i < len-1) {
+        if (str.charAt(i-1) == str.charAt(i+1))
+          found = true;
+        else
+          found = false;
+      }
+    }
+    return found;
+  }
+
 }
--- src/test/java/String02Test/SameStarCharTest.java
+++ src/test/java/String02Test/SameStarCharTest.java
@@ -1,4 +1,54 @@
 package String02Test;
 
+import String02.SameStarChar;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class SameStarCharTest {
+   /* sameStarChar("xy*yzz") → true
+    * sameStarChar("xy*zzz") → false
+    * sameStarChar("*xa*az") → true
+    **/
+
+  @Test
+  public void sampleTest_one() {
+    SameStarChar ssc = new SameStarChar();
+    Assert.assertTrue(ssc.sameStarChar("xy*yzz"));
+  }
+
+  @Test
+  public void sampleTest_two() {
+    SameStarChar ssc = new SameStarChar();
+    Assert.assertTrue(ssc.sameStarChar("*xa*az"));
+  }
+
+  @Test
+  public void sampleTest_four() {
+    SameStarChar ssc = new SameStarChar();
+    Assert.assertFalse(ssc.sameStarChar("xy*zzz"));
+  }
+
+  @Test
+  public void sampleTest_five() {
+    SameStarChar ssc = new SameStarChar();
+    Assert.assertFalse(ssc.sameStarChar("XY*YYYY*Z*"));
+  }
+
+  @Test
+  public void sampleTest_six() {
+    SameStarChar ssc = new SameStarChar();
+    Assert.assertTrue(ssc.sameStarChar(""));
+  }
+
+  @Test
+  public void sampleTest_seven() {
+    SameStarChar ssc = new SameStarChar();
+    Assert.assertTrue(ssc.sameStarChar("*"));
+  }
+
+  @Test
+  public void sampleTest_eight() {
+    SameStarChar ssc = new SameStarChar();
+    Assert.assertTrue(ssc.sameStarChar("**"));
+  }
 }
