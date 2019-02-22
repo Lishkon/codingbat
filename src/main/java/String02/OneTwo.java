61a74289d1e9a26d9a3d13ebdaf4fae26dd5d77e
lishkon <lishkon@gmail.com>
2019-02-22 17:55:21 +0200

    OneTwo is solved

--- src/main/java/String02/OneTwo.java
+++ src/main/java/String02/OneTwo.java
@@ -1,4 +1,29 @@
 package String02;
 
 public class OneTwo {
+  /**
+   * Given a string, compute a new string by moving the first char to come after the next two chars,
+   * so "abc" yields "bca". Repeat this process for each subsequent group of 3 chars, so "abcdef" yields "bcaefd".
+   * Ignore any group of fewer than 3 chars at the end.
+   *
+   *
+   * oneTwo("abc") → "bca"
+   * oneTwo("tca") → "cat"
+   * oneTwo("tcagdo") → "catdog"
+   *
+   * solved thanks to guys here: https://stackoverflow.com/questions/41434303/how-to-tackle-the-codingbat-string-2-onetwo-challenge
+   */
+
+  public String oneTwo(String str) {
+
+    String res = "";
+    if (str.length() > 2) {
+      for(int i=0; i<str.length()-2; i+=3) {
+        res += str.substring(i+1,i+3)+str.charAt(i);
+      }
+    }
+
+    return res;
+  }
+
 }
--- src/test/java/String02Test/OneTwoTest.java
+++ src/test/java/String02Test/OneTwoTest.java
@@ -1,4 +1,53 @@
 package String02Test;
 
+import String02.OneTwo;
+import org.junit.Assert;
+import org.junit.Test;
+
+/* oneTwo("abc") → "bca"
+ * oneTwo("tca") → "cat"
+ * oneTwo("tcagdo") → "catdog"
+ */
 public class OneTwoTest {
+  @Test
+  public void sampleTest_one() {
+    OneTwo ot = new OneTwo();
+    Assert.assertEquals("bca", ot.oneTwo("abc"));
+  }
+
+  @Test
+  public void sampleTest_two() {
+    OneTwo ot = new OneTwo();
+    Assert.assertEquals("", ot.oneTwo("bc"));
+  }
+
+  @Test
+  public void sampleTest_three() {
+    OneTwo ot = new OneTwo();
+    Assert.assertEquals("cat", ot.oneTwo("tca"));
+  }
+
+  @Test
+  public void sampleTest_four() {
+    OneTwo ot = new OneTwo();
+    Assert.assertEquals("catdog", ot.oneTwo("tcagdo"));
+  }
+
+  @Test
+  public void sampleTest_five() {
+    OneTwo ot = new OneTwo();
+    Assert.assertEquals("leatex", ot.oneTwo("alextest"));
+  }
+
+  @Test
+  public void sampleTest_six() {
+    OneTwo ot = new OneTwo();
+    Assert.assertEquals("", ot.oneTwo(""));
+  }
+
+  @Test
+  public void sampleTest_seven() {
+    OneTwo ot = new OneTwo();
+    Assert.assertEquals("", ot.oneTwo("x"));
+  }
 }
