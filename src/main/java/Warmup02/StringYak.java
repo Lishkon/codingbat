778f134595248db246b325ee8602b927c4b4044f
lishkon <lishkon@gmail.com>
2019-12-06 01:26:29 +0200

    StringYak is solved

--- src/main/java/Warmup02/StringYak.java
+++ src/main/java/Warmup02/StringYak.java
@@ -1,4 +1,17 @@
 package Warmup02;
 
 public class StringYak {
+    /*
+        stringYak("yakpak") → "pak"
+        stringYak("pakyak") → "pak"
+        stringYak("yak123ya") → "123ya"
+     */
+    // This code only works for non-case-sensetive input data. It won't work with inputs like "YakPak".
+    public String stringYak(String str) {
+        String output = "";
+        if (str.contains("yak")) {
+            output = str.replaceAll("yak","");
+        }
+        return  output;
+    }
 }
--- src/test/java/Warmup02Test/StringYakTest.java
+++ src/test/java/Warmup02Test/StringYakTest.java
@@ -1,4 +1,39 @@
 package Warmup02Test;
 
+import Warmup02.AllPairs;
+import Warmup02.StringYak;
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+
 public class StringYakTest {
+    /*
+        stringYak("yakpak") → "pak"
+        stringYak("pakyak") → "pak"
+        stringYak("yak123ya") → "123ya"
+     */
+    @Test
+    public void yakPakTest() {
+        StringYak sy = new StringYak();
+        Assert.assertEquals("pak", sy.stringYak("yakpak"));
+    }
+
+    @Test
+    public void pakYakTest() {
+        StringYak sy = new StringYak();
+        Assert.assertEquals("pak", sy.stringYak("pakyak"));
+    }
+
+    @Test
+    public void yak123yaTest() {
+        StringYak sy = new StringYak();
+        Assert.assertEquals("123ya", sy.stringYak("yak123ya"));
+    }
+
+    @Test
+    @Ignore
+    public void yakYakYakPakTest() {
+        StringYak sy = new StringYak();
+        Assert.assertEquals("Pak", sy.stringYak("yakYakYakPak"));
+    }
 }
