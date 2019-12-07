3865c75918e7b790d177b33716722abaef44a882
lishkon <lishkon@gmail.com>
2019-12-08 00:34:17 +0200

    StringBits is resolved

--- src/main/java/Warmup02/StringBits.java
+++ src/main/java/Warmup02/StringBits.java
@@ -8,7 +8,7 @@ public class StringBits {
      */
     public String stringBits(String str) {
         String output = "";
-        for (int i = 0; i <= str.length(); i += 2) {
+        for (int i = 0; i < str.length(); i += 2) {
             output += str.substring(i, i+1);
         }
         return output;
