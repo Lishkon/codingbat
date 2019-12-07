4e19c0ecffe7e2feb892bd9931b53b9575efeb1a
lishkon <lishkon@gmail.com>
2019-12-08 01:09:33 +0200

    CountXX is solved

--- src/test/java/Warmup02Test/CountXXTest.java
+++ src/test/java/Warmup02Test/CountXXTest.java
@@ -1,4 +1,31 @@
 package Warmup02Test;
 
+import Warmup02.CountXX;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CountXXTest {
+    /*
+            countXX("abcxx") → 1
+            countXX("xxx") → 2
+            countXX("xxxx") → 3
+     */
+    @Test
+    public void abcxxTest() {
+        CountXX cx = new CountXX();
+        Assert.assertEquals(1, cx.countXX("abcxx"));
+    }
+
+    @Test
+    public void xxxTest() {
+        CountXX cx = new CountXX();
+        Assert.assertEquals(2, cx.countXX("xxx"));
+    }
+
+    @Test
+    public void xxxxTest() {
+        CountXX cx = new CountXX();
+        Assert.assertEquals(3, cx.countXX("xxxx"));
+    }
+
 }
